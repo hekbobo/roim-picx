e212c9bb240157966bfe55674d20607614182296
zyb <zyb@shoot.net.cn>
2024-06-14 13:04:33 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -56,9 +56,9 @@ import { useRouter } from 'vue-router'
 import { ElScrollbar, ElConfigProvider } from 'element-plus'
 import zhCn from "element-plus/es/locale/lang/zh-cn"
 
-const repoLink = 'https://www.baidu.com'
-const repoName = '图床管理工具'
-const appName = '图床管理工具'
+const repoLink = 'https://file.zyb666.xyz/'
+const repoName = '文件管理工具'
+const appName = '文件管理工具'
 
 document.title = appName
 
